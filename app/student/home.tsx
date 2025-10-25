620a2cf6536a792eb9ee33fe22661f47f074d4cb
gfwells <153139012+hyperioncodes@users.noreply.github.com>
2025-10-25 13:21:44 -0500

    finished with home.tsx right now... will add missing parts when done with calendar, bulletin, etc. added heightPercentage and just added bulletin.tsx

--- app/student/home.tsx
+++ app/student/home.tsx
@@ -5,6 +5,8 @@ import { MaterialIcons } from "@expo/vector-icons";
 import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
 import Box from "@/components/Box";
 import notificationsObject from "@/types/notificationsFrom";
+import {useWindowDimensions} from 'react-native';
+import heightPercentage from "@/utils/heightPercentage";
 type Props = {
   name: string;
   notifications: number;
@@ -14,80 +16,18 @@ type Props = {
 const PANEL_WIDTH = 350;
 
 export default function StudentHome() {
-const name = "Toby"
-const notifications_from:notificationsObject[] = [{message:"Hi",from:"8m",type:"chat",routeTo:"/"}]
-const notifications = 1;
-    const insets = useSafeAreaInsets()
-    const router = useRouter()
-  const [isOpen, setOpen] = useState(false);
-  const slideAnim = useRef(new Animated.Value(PANEL_WIDTH)).current; // Off-screen to the right
-  const navigation = useNavigation();
-  const [notificationsNum, setNum] = useState(notifications);
-
-  const toggleNotifications = () => {
-    const nextIsOpen = !isOpen;
-    setOpen(nextIsOpen);
-
-    Animated.timing(slideAnim, {
-      toValue: nextIsOpen ? 0 : PANEL_WIDTH,
-      duration: 300,
-      useNativeDriver: false,
-    }).start();
-  };
-
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <View style={{ marginRight: 20 }}>
-          <Pressable onPress={toggleNotifications} style={{ width: 30, height: 30 }}>
-            <MaterialIcons name="notifications" size={30} color="white" />
-            {notificationsNum > 0 && (
-              <View style={styles.badge}>
-                <Text style={styles.badgeText}>{notificationsNum}</Text>
-              </View>
-            )}
-          </Pressable>
-        </View>
-      ),
-    });
-  }, [navigation, toggleNotifications, notificationsNum,notifications_from]);
-
-  return (
-    <View style={styles.main}>
-      <Text style={styles.greeting}>Welcome back, {name}</Text>
-      <Text>Upcoming:</Text>
-
-      
-      
-      <Animated.View
-        style={[
-          styles.notificationsPanel,
-          { transform: [{ translateX: slideAnim }],
-          
-        }, // Slide in from right
-        ]}
-      >
-        
-        
-        {notifications_from.map((element,index)=>(
-            <View key={index}>
-                <Pressable onPress={(()=>router.replace(element.routeTo as any))}>
-                <Box message={element.message} from={"8m"} type={element.type}/>
-                </Pressable>
-            </View>
-        ))}
-        
-      </Animated.View>
-      
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
+  const styles = StyleSheet.create({
   main: {
     flex: 1,
     backgroundColor: "#fff",
-    width:"100%"
+    width:"100%",
+    
+  },
+  upcoming:{
+    fontFamily:"Nunito",
+    fontSize:24,
+    textAlign:"center",
+    marginTop:heightPercentage(0.20)
   },
   greeting: {
     fontSize: 50,
@@ -133,3 +73,73 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
 });
+
+const name = "Toby"
+const notifications_from:notificationsObject[] = [{message:"Hi",from:"8m",type:"chat",routeTo:"/"}]
+const notifications = 1;
+    const insets = useSafeAreaInsets()
+    const router = useRouter()
+  const [isOpen, setOpen] = useState(false);
+  const slideAnim = useRef(new Animated.Value(PANEL_WIDTH)).current; // Off-screen to the right
+  const navigation = useNavigation();
+  const [notificationsNum, setNum] = useState(notifications);
+
+  const toggleNotifications = () => {
+    const nextIsOpen = !isOpen;
+    setOpen(nextIsOpen);
+
+    Animated.timing(slideAnim, {
+      toValue: nextIsOpen ? 0 : PANEL_WIDTH,
+      duration: 300,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <View style={{ marginRight: 20 }}>
+          <Pressable onPress={toggleNotifications} style={{ width: 30, height: 30 }}>
+            <MaterialIcons name="notifications" size={30} color="white" />
+            {notificationsNum > 0 && (
+              <View style={styles.badge}>
+                <Text style={styles.badgeText}>{notificationsNum}</Text>
+              </View>
+            )}
+          </Pressable>
+        </View>
+      ),
+    });
+  }, [navigation, toggleNotifications, notificationsNum,notifications_from]);
+
+  return (
+    <View style={styles.main}>
+      <Text style={styles.greeting}>Welcome back, {name}</Text>
+      <Text style={styles.upcoming}>Upcoming:</Text>
+      
+      
+      {/*Notifications panel*/}
+      <Animated.View
+        style={[
+          styles.notificationsPanel,
+          { transform: [{ translateX: slideAnim }],
+          
+        }, // Slide in from right
+        ]}
+      >
+        
+        
+        {notifications_from.map((element,index)=>(
+            <View key={index}>
+                <Pressable onPress={(()=>router.replace(element.routeTo as any))}>
+                <Box message={element.message} from={element.from} type={element.type}/>
+                </Pressable>
+            </View>
+        ))}
+        
+      </Animated.View>
+      </View>
+  );
+}
+
+
---
+++ utils/heightPercentage.ts
@@ -0,0 +1,6 @@
+import { useWindowDimensions } from "react-native";
+
+export default function heightPercentage(percentage:number){
+const {height} = useWindowDimensions()
+return height*percentage
+}
